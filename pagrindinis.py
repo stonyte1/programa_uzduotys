baf788c74908625d876a8db3896dc7a6522ba008
KarolisTam <karolistamu@gmail.com>
2023-04-14 15:43:31 +0300

    kuras

--- pagrindinis.py
+++ pagrindinis.py
@@ -1,1 +1,15 @@
-uzduotys = {'uzduotis1': 'nepadaryta', 'uzduotis2': 'nepadaryta', 'uzduotis3': 'nepadaryta'}
+balansas = 
+while True:
+    print('Porgramu pasirinkimas: Kuras(1), Blanasas(2), Buto nuoma(3), Dienos islaidos(4), Pramogos(5)')
+    pasirinkimas = int(input("Iveskite pageidautina programa: "))
+    if pasirinkimas == 1:
+        kuras_kaina = 1.5
+        sunaudotas_kuras = int(input("Jusu masinos sunaudotas kuras litrais per menesi"))
+        kuro_islaidos = kuras_kaina * sunaudotas_kuras
+        balansas -= kuro_islaidos
+        print(f"Jusu kuro islaidos: {kuro_islaudos}")
+    elif pasirinkimas == 2:
+    elif pasirinkimas == 3:
+    elif pasirinkimas == 4:
+    elif pasirinkimas == 5
+
