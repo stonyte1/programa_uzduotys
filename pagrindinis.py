e6898ea35b460d8fda54daf475164b27a06d2662
KarolisTam <karolistamu@gmail.com>
2023-04-17 09:03:02 +0300

    Reikia dekoraciju

--- pagrindinis.py
+++ pagrindinis.py
@@ -1,33 +1,49 @@
 zurnalas = []
-
+print('-----Sveiki atvykę į išlaidus suvestinę-----')
 atlyginimas = float(input("Įvesti šio men. atlyginimą: "))
+print('--------------------------------------------')
 atlyginimo_mokestis_proc = float(input("Įvesti mokeščio procentų dydį: "))
-
+print('--------------------------------------------')
 balansas = atlyginimas * (1 - atlyginimo_mokestis_proc / 100)
 zurnalas.append(balansas)
 
 
 while True:
 
-    print('Porgramų pasirinkimas: Kuras(1), Blanasas(2), Buto nuoma(3), Dienos išlaidos(4), Pramogos(5), Iseiti(6)')
+    print('Porgramų pasirinkimas: Kuras(1), Blanasas(2), Buto nuoma(3), Dienos išlaidos(4), Pramogos(5), Zurnalas(6), Iseiti(9)')
     pasirinkimas = int(input("Įveskite pageidautina programą: "))
-
+    print('-----------------------------------------------')
     if pasirinkimas == 1:
         kuras = int(input("Įveskite suma kuria išleidžiate kurui: "))
         zurnalas.append(-kuras)
         print(f"Jūsų kuro išlaidos: {kuras}")
+        print('--------------------------------------------')
 
     elif pasirinkimas == 2:
-        print(f'Jūsu dabartinis balansas: {balansas}')
+        print(f'Jūsu dabartinis balansas: {balansas} Eurų.')
+        print('--------------------------------------------')
 
     elif pasirinkimas == 3:
         butas = int(input('Įveskite buto nuomos išlaidas: '))
+        print('--------------------------------------------')
         zurnalas.append(-butas)
-        print(butas)
+
     elif pasirinkimas == 4:
-        pass
+        dienos_islaidos = int(input('Įveskite dienos išlaidas: '))
+        print('--------------------------------------------')
+        zurnalas.append(-dienos_islaidos)
+
     elif pasirinkimas == 5:
-        pass
+        pramogos = int(input('Įveskite progomų išlaidas: '))
+        print('--------------------------------------------')
+        zurnalas.append(-pramogos)
     elif pasirinkimas == 6:
-        print('Geros dienos')
+        bendras = 0
+        for irasas in zurnalas:
+            print(irasas)
+            bendras += irasas
+        print(f'balansas: {bendras}')
+        print('--------------------------------------------')
+    elif pasirinkimas == 9:
+        print('-------Geros dienos--------')
         break
