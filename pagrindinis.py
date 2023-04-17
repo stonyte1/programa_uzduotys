4499d067050533597baa0c4696ee3d94f46e7f90
Evelina Stonyte <stonyteevelina@gmail.com>
2023-04-17 09:13:05 +0300

    Update

--- pagrindinis.py
+++ pagrindinis.py
@@ -1,18 +1,21 @@
 zurnalas = []
 print('-----Sveiki atvykę į išlaidus suvestinę-----')
+
 atlyginimas = float(input("Įvesti šio men. atlyginimą: "))
 print('--------------------------------------------')
+
 atlyginimo_mokestis_proc = float(input("Įvesti mokeščio procentų dydį: "))
 print('--------------------------------------------')
+
 balansas = atlyginimas * (1 - atlyginimo_mokestis_proc / 100)
 zurnalas.append(balansas)
-
 
 while True:
 
     print('Porgramų pasirinkimas: Kuras(1), Blanasas(2), Buto nuoma(3), Dienos išlaidos(4), Pramogos(5), Zurnalas(6), Iseiti(9)')
     pasirinkimas = int(input("Įveskite pageidautina programą: "))
     print('-----------------------------------------------')
+
     if pasirinkimas == 1:
         kuras = int(input("Įveskite suma kuria išleidžiate kurui: "))
         zurnalas.append(-kuras)
@@ -34,7 +37,7 @@ while True:
         zurnalas.append(-dienos_islaidos)
 
     elif pasirinkimas == 5:
-        pramogos = int(input('Įveskite progomų išlaidas: '))
+        pramogos = int(input('Įveskite pramogų išlaidas: '))
         print('--------------------------------------------')
         zurnalas.append(-pramogos)
     elif pasirinkimas == 6:
